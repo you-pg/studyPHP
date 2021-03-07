82ad6daf400960764dec87133cea2fd50affa049
you <you.you.pg@gmail.com>
2021-03-07 12:19:01 +0900

    タイトル編集

--- choose.php
+++ choose.php
@@ -10,7 +10,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link href="https://fonts.googleapis.com/earlyaccess/nicomoji.css" rel="stylesheet">
 <link rel="stylesheet" href="css/diagnosis.css">
-<title>職業診断</title>
+<title>職業診断(PHP)</title>
 
 </head>
 <body>
